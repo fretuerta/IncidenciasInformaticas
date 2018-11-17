5fe57619926270cab98d81a47e2771e09b8b5362
Fernando Retuerta <fernando.retuerta@gmail.com>
2018-11-17 19:11:53 +0100

    Actualización de texto e iconos en menús.

--- app/src/main/java/com/retuerta/fernando/incidenciasinformaticas/MainActivity.java
+++ app/src/main/java/com/retuerta/fernando/incidenciasinformaticas/MainActivity.java
@@ -80,17 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.lista_incidencias) {
+            // gestionar los eventos de los botones aquí
+        } else if (id == R.id.gestion_incidencias) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.lista_usuarios) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.gestion_usuarios) {
 
         }
 
